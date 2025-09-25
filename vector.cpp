e93510de4b4e5ff0cb78051036c4a03614bc12bb
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-09-25 23:47:00 +0200

    General: Fixed vector extraction

--- vector.cpp
+++ vector.cpp
@@ -1,21 +1,18 @@
-class Vector {
-public:
-    float x,y;
+#include "vector.h"
 
-    Vector(const float x, const float y) {
-        this->x = x;
-        this->y = y;
-    }
+Vector::Vector(const float x, const float y) {
+    this->x = x;
+    this->y = y;
+}
 
-    Vector operator+(Vector& other) const {
-        return Vector{this->x+other.x, this->y+other.y};
-    }
+Vector Vector::operator+(Vector& other) const {
+    return Vector{this->x+other.x, this->y+other.y};
+}
 
-    Vector operator-(Vector& other) const {
-        return Vector{this->x-other.x, this->y-other.y};
-    }
+Vector Vector::operator-(Vector& other) const {
+    return Vector{this->x-other.x, this->y-other.y};
+}
 
-    Vector operator*(float scalar) const {
-        return Vector{this->x*scalar, this->y*scalar};
-    }
-};
+Vector Vector::operator*(float scalar) const {
+    return Vector{this->x*scalar, this->y*scalar};
+}
