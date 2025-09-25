6cf6593d00dd281b41ac52c1bc6180b8c69b684b
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-09-25 20:56:00 +0200

    main: Introduced vector operations

--- main.cpp
+++ main.cpp
@@ -1,29 +1,6 @@
 #include <chrono>
-#include <iostream>
 #include <bits/this_thread_sleep.h>
 #include <SFML/Graphics.hpp>
-
-
-class PlayerRect {
-public:
-    float x,y;
-    sf::RectangleShape visuals;
-
-    PlayerRect(const float x,const float y) {
-        this->x = x;
-        this->y = y;
-        this->visuals = sf::RectangleShape{sf::Vector2f(25.f, 150.f)};
-        this->visuals.setPosition(x,y);
-    }
-
-    void move(const float deltaX,const float deltaY) {
-        this->x += deltaX;
-        this->y += deltaY;
-        this->visuals.setPosition(this->x, this->y);
-    }
-};
-
-
 
 class Vector {
 public:
@@ -33,6 +10,37 @@ public:
         this->x = x;
         this->y = y;
     }
+
+    Vector operator+(Vector& other) const {
+        return Vector{this->x+other.x, this->y+other.y};
+    }
+
+    Vector operator-(Vector& other) const {
+        return Vector{this->x-other.x, this->y-other.y};
+    }
+
+    Vector operator*(float scalar) const {
+        return Vector{this->x*scalar, this->y*scalar};
+    }
+};
+
+
+class PlayerRect {
+public:
+    Vector pos{0,0};
+    Vector velocity{0,0};
+    sf::RectangleShape visual{sf::Vector2f(12.f,100.f)};
+
+    PlayerRect(const float x,const float y) {
+        this->pos.x = x;
+        this->pos.y = y;
+        this->visual.setPosition(x,y);
+    }
+
+    void move() {
+        pos = pos+velocity;
+        visual.setPosition(pos.x, pos.y);
+    }
 };
 
 class DottedLine {
@@ -41,10 +49,28 @@ public:
 
     DottedLine(const float x, const float ystart, const float yend, const float interval) {
         for (float y = ystart; y < yend; y += interval) {
-            sf::RectangleShape rect{sf::Vector2f(25.f, 25.f)};
+            sf::RectangleShape rect{sf::Vector2f(20.f, 20.f)};
             rect.setPosition(x, y);
             this->visuals.push_back(rect);
         }
+    }
+};
+
+class Ball {
+public:
+    Vector position{0,0};
+    Vector velocity{0,0};
+    sf::CircleShape visual{12};
+
+    Ball() {
+        position.x = 100;
+        position.y = 100;
+        velocity.x = 0.05;
+    }
+
+    void move() {
+        position = position + velocity;
+        this->visual.setPosition(position.x, position.y);
     }
 };
 
@@ -55,13 +81,13 @@ int main() {
 
     PlayerRect p1{100,0};
     PlayerRect p2{1080-100, 200};
-    DottedLine line{1080/2, 0, 600, 50};
-    Vector velocity{0,0};
+    DottedLine line{1080/2.f, 0, 600, 50};
+    Ball ball;
 
     while (window.isOpen()) {
         auto frameStart = std::chrono::steady_clock::now();
 
-        // Handle event
+        // 1 | Handle events
         sf::Event event{};
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
@@ -69,26 +95,31 @@ int main() {
             }
 
             if (event.type == sf::Event::KeyPressed) {
-                std::cout << "Registered Key press!" << "\n";
-                if (event.key.code == sf::Keyboard::Up)    { velocity.y = -1; }
-                if (event.key.code == sf::Keyboard::Down)  { velocity.y = 1; }
+                if (event.key.code == sf::Keyboard::W)    p1.velocity.y = -1.f;
+                if (event.key.code == sf::Keyboard::S)    p1.velocity.y = 1.f;
+                if (event.key.code == sf::Keyboard::Up)   p2.velocity.y = -1.f;
+                if (event.key.code == sf::Keyboard::Down) p2.velocity.y = 1.f;
             }
 
             if (event.type == sf::Event::KeyReleased) {
-                std::cout << "Registered Key release!" << "\n";
-                if (event.key.code == sf::Keyboard::Up)    { velocity.y = 0; }
-                if (event.key.code == sf::Keyboard::Down)  { velocity.y = 0; }
+                if (event.key.code == sf::Keyboard::W || event.key.code == sf::Keyboard::S)
+                    p1.velocity.y = 0;
+                if (event.key.code == sf::Keyboard::Up || event.key.code == sf::Keyboard::Down)
+                    p2.velocity.y = 0;
             }
         }
 
         // 2 | Update model
-        p1.move(0.2*velocity.x, 0.2*velocity.y);
+        p1.move();
+        p2.move();
+        ball.move();
 
         // 3 | Render frame
         window.clear(sf::Color::Transparent);
-        window.draw(p1.visuals);
-        window.draw(p2.visuals);
-        int N_DOTS = line.visuals.size();
+        window.draw(p1.visual);
+        window.draw(p2.visual);
+        window.draw(ball.visual);
+        std::size_t N_DOTS = line.visuals.size();
         for (int j = 0; j < N_DOTS; j++) {
             window.draw(line.visuals[j]);
         }
